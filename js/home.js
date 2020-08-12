f7e8f918035fc558a06c11a454cb4c5d293636a3
tckmn <andy@tck.mn>
2020-08-12 13:04:27 -0500

    better seq'd animations, add basic puzzle anim

--- js/home.js
+++ js/home.js
@@ -27,36 +27,52 @@ window.addEventListener('load', () => {
         var gs = elt.getElementsByTagName('g'), g1 = gs[0], g2 = gs[1];
         // squish small/large, zoom out
         var ss = 0.7, sl = 1.8, zo = 0.5;
-        return ts => {
-            var phase = Math.floor(ts*3), t = (ts * 3) % 1;
-            switch (phase) {
-            case 0:
+        return animSeq([1/3, 1/3, 1/3], [
+            t => {
                 g1.setAttribute('transform', `rotate(${tw(0, -20, t)}) scale(${tw(1, zo, t)} ${tw(1, ss*zo, t)}) translate(${tw(0, -1.5, t)} ${tw(0, 0.5, t)})`);
-                break;
-            case 1:
+            },
+            t => {
                 g1.setAttribute('transform', `rotate(${-20}) scale(${zo} ${tw(ss*zo, sl*zo, t)}) translate(${tw(-1.5, -2, t)} ${tw(0.5, -1.5, t)})`);
                 g2.setAttribute('transform', `rotate(${20})  scale(${zo} ${tw(sl*zo, ss*zo, t)}) translate(${tw(2, 1.5, t)}   ${tw(-1.5, 0.5, t)})`);
                 g1.setAttribute('opacity', 1-t);
                 g2.setAttribute('opacity', t);
-                break;
-            case 2:
+            },
+            t => {
                 g1.setAttribute('opacity', 0);
                 g2.setAttribute('opacity', 1); // oops
                 g2.setAttribute('transform', `rotate(${tw(20, 0, t)}) scale(${tw(zo, 1, t)} ${tw(ss*zo, 1, t)}) translate(${tw(1.5, 0, t)} ${tw(0.5, 0, t)})`);
-                break;
-            case 3:
+            },
+            t => {
                 g1.setAttribute('transform', '');
                 g1.setAttribute('opacity', 1);
                 g2.setAttribute('opacity', 0);
-                break;
             }
-        };
+        ]);
     });
 
     anim(document.getElementById('home_puzzle'), 600, elt => {
         var ps = elt.getElementsByTagName('path'), p1 = ps[0], p2 = ps[1];
-        return ts => {
-        };
+        // zoom out, translation
+        var zo = 0.25, tr = 0.77;
+        return animSeq([1/3, 1/3, 1/3], [
+            t => {
+                p1.setAttribute('transform', `translate(${-t}, 0) scale(${1-t*zo})`);
+                p2.setAttribute('transform', `translate(${t}, 0)  scale(${1-t*zo})`);
+            },
+            t => {
+                p1.setAttribute('transform', `translate(${tw(-1, -tr*(1-zo), t)}, 0) scale(${1-zo})`);
+                p2.setAttribute('transform', `translate(${tw(1,   tr*(1-zo), t)}, 0) scale(${1-zo})`);
+            },
+            t => {
+                p1.setAttribute('transform', `translate(${tw(-tr*(1-zo), 0, t)}, 0)    scale(${1-zo+zo*t})`);
+                p2.setAttribute('transform', `translate(${tw( tr*(1-zo), tr*2, t)}, 0) scale(${1-zo+zo*t})`);
+                p2.setAttribute('opacity', 1-t);
+            },
+            t => {
+                p2.setAttribute('opacity', 1);
+                p2.setAttribute('transform', 'translate(10, 0)');
+            }
+        ]);
     });
 
 });
@@ -75,8 +91,14 @@ function anim(elt, dur, fn) {
     });
 }
 
-function animSeq(ts, durs, anims) {
-
+function animSeq(durs, anims) {
+    return ts => {
+        if (ts == 1) return anims[anims.length-1](1);
+        for (var i = 0; i < anims.length; ++i) {
+            if (ts < durs[i]) return anims[i](ts / durs[i]);
+            else ts -= durs[i];
+        }
+    };
 }
 
 function tw(a, b, t) { return a + (b-a)*t; }
